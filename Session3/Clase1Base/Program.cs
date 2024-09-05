7f0fae87256204118dd751941adad504f19078c7
Romulo Meregildo <mmeregildo@gmail.com>
2024-09-04 20:33:25 -0500

    subida de viosual

--- Session3/Clase1Base/Program.cs
+++ Session3/Clase1Base/Program.cs
@@ -6,13 +6,18 @@ long altura = 21;
 string nombre = "Manuel";
 decimal montoEnBanco = 1236544556.5m;
 double dinero = 1651.36;
+float danio = 124.4f;
 char letra= 'A';
 bool tieneGorro= false;
-
+var kilometros = 545454.4544;
 Console.WriteLine("Escribe tu nombre");
 nombre=Console.ReadLine();
 Console.WriteLine("Mi nombre es "+nombre+" y mi edad es " +edad);
 Console.WriteLine($"Mi altura es {altura}");
 Console.WriteLine("escrib e tu dinero");
+//este es com entyario de prueba
+/*
+ * comentrarioi largo
+ */
 dinero =double.Parse(Console.ReadLine());
 Console.WriteLine($"tu dinero es{dinero}");
